77a6c05caf4400d6bd7a4a7bb07e8bfa7c9f68e2
Surya V <mrstudieshelper@gmail.com>
2026-06-01 22:43:12 +0530

    Time: 7 ms (8.24%), Space: 9.3 MB (46.35%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,15 @@
+int reverse(int num){
+    int digit;
+    int reversed = 0;
+    while(num!=0) {
+        digit = num % 10;
+
+        if( reversed > INT_MAX / 10 || reversed < INT_MIN / 10 ) {
+            return 0;
+        }
+
+        reversed = (reversed*10) + digit;
+        num = num / 10;
+    }
+    return reversed;
+}
