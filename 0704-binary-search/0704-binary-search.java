35bc83cc4408410aaa52cbade240ada51c7eb176
Surya V <mrstudieshelper@gmail.com>
2026-06-06 22:19:50 +0530

    Time: 0 ms (100%), Space: 48.2 MB (67.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,24 +1,23 @@
 class Solution {
+
     public int RecursiveBinarySearch(int[] nums, int low, int high, int target) {
         if(low>high) return -1;
 
-        int mid = (low + high) / 2;
+        int mid = (low + high )/ 2;
 
         if(nums[mid] == target) return mid;
-        else if(target > nums[mid]) {
-            return RecursiveBinarySearch(nums,mid+1,high,target);
+        else if ( target > nums[mid])  {
+            return RecursiveBinarySearch(nums, mid + 1, high, target );
         }
-        else {
-            return RecursiveBinarySearch(nums,low,mid-1,target);
-        }
+        else return RecursiveBinarySearch(nums, low, mid-1, target );
     }
 
     public int search(int[] nums, int target) {
-        int n = nums.length ;
+        int n = nums.length;
         int low = 0;
-        int high = n-1 ;
-        int index;
-        index = RecursiveBinarySearch(nums,low,high,target);
+        int high = n-1;
+
+        int index = RecursiveBinarySearch(nums,low,high,target);
         return index;
     }
 }
