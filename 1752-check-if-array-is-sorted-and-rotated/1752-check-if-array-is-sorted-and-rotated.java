923dc97830451743a54468c17a51e7bd00b58d77
Surya V <mrstudieshelper@gmail.com>
2026-06-12 20:09:29 +0530

    Time: 0 ms (100%), Space: 42.8 MB (76.76%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -4,17 +4,19 @@ class Solution {
         if(n==1) return true;
 
         int pos = 1;
-        while( pos<n && nums[pos] >= nums[pos-1]) {
-            pos++ ;
+        while(pos<n && nums[pos] >= nums[pos-1] ) {
+            pos = pos + 1;
+        } 
+
+        if (pos == n) return true;
+        if (nums[pos] > nums[0] ) return false;
+        
+
+        pos = pos + 1;
+
+        while(pos<n && nums[pos] <= nums[0] && nums[pos]>= nums[pos-1]) {
+            pos = pos + 1;
         }
-        if (pos==n) return true;
-        if ( nums[pos] > nums[0] )  return false;
-        if (pos == n-1) return true;
-        //
-        pos++ ;
-        while(pos<n && nums[pos]<=nums[0] && nums[pos]>=nums[pos-1] ) {
-            pos++ ;
-        }
-        return n==pos ;
+        return pos == n;
     }
 }
