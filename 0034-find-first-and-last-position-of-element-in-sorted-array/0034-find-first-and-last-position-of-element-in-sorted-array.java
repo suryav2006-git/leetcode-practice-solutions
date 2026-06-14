4b43086182e93f882f84a5e20496fe497251f0ca
Surya V <mrstudieshelper@gmail.com>
2026-06-14 09:40:32 +0530

    Time: 0 ms (100%), Space: 48.3 MB (13.41%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -6,12 +6,12 @@ class Solution {
         int first = -1;
 
         while(low<=high) {
-            int mid = (low + high) / 2;
-            if(nums[mid] == target) {
+            int mid = low + (high - low)/2;
+            if(nums[mid]==target) {
                 first = mid;
-                high = mid-1 ;
+                high = mid - 1;
             }
-            else if (nums[mid] >= target) {
+            else if (nums[mid]>=target) {
                 high = mid - 1;
             }
             else low = mid + 1;
@@ -19,35 +19,36 @@ class Solution {
         return first;
     }
 
-    public int SecondSearch(int[] nums, int n, int target) {
-        int low = 0;
+    public int SecondSearch(int[] nums, int n , int target) {
+        int low =0;
         int high = n-1;
         int second = -1;
 
         while(low<=high) {
-            int mid = (low + high) / 2;
+            int mid = low + (high - low ) / 2;
+
             if(nums[mid] == target) {
                 second = mid;
-                low = mid+1;
-            }
-            else if(nums[mid] > target) {
+                low = mid + 1;
+            }  
+            else if(nums[mid]>target) {
                 high = mid -1;
             }
-            else low = mid+1;
+            else low = mid + 1;
         }
         return second;
-    } 
+    }
 
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
         int first = -1;
-        int second;
+        int second = -1;
 
-        first = FirstSearch(nums, n, target);
-        if(first== -1) return new int[] {-1 , -1};
+        first = FirstSearch(nums,n,target);
 
+        if(first == -1) return new int[] {-1,-1};
         second = SecondSearch(nums,n,target);
-        
-        return new int[] {first, second};
+
+        return new int[] {first,second};
     }
 }
