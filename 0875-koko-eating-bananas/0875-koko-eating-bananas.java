997a5af4f1606ec4c1ba23d1c17e2a47acb4ff7f
Surya V <mrstudieshelper@gmail.com>
2026-06-19 20:03:18 +0530

    Time: 25 ms (24.41%), Space: 48.5 MB (9.66%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public int maxof(int[] piles) {
+        int maxi = Integer.MIN_VALUE ;
+        int n = piles.length;
+
+        for(int i=0; i<n; i++) {
+            if(piles[i] >= maxi) {
+                maxi = piles[i];
+            }
+        }
+        return maxi;
+    }
+    public int totalhrsof(int[] piles, int hour) {
+        int totalhrs = 0;
+        int n = piles.length;
+
+        for(int i=0; i<n ; i++) {
+            totalhrs += Math.ceil( (double) piles[i] / hour);
+        }
+        return totalhrs;
+    }
+    public int minEatingSpeed(int[] piles, int hours) {
+        int low = 0;
+        int high = maxof(piles);
+
+        while(low<=high) {
+            int mid = low + (high - low) / 2;
+            int totalhrs = totalhrsof(piles, mid);
+
+            if(totalhrs <= hours) {
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return low;
+    }
+}
