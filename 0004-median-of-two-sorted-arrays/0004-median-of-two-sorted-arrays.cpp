8ca91ce029d69085dd17db64aa788b7f17303e59
Surya V <mrstudieshelper@gmail.com>
2026-07-03 23:50:46 +0530

    Time: 19 ms (6.09%), Space: 95.2 MB (61.68%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,50 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-    int n1 = a.size();
-    int n2 = b.size();
-    int i=0, j=0;
-    int count = 0;
-    int n= n1 + n2;
-    int ind2 = n / 2;
-    int ind1 = ind2-1;
+        int n1 = a.size();
+        int n2 = b.size();
 
-    int ind1ele = -1;
-    int ind2ele = -1;
+        if(n1 > n2) return findMedianSortedArrays(b,a);
+        int n = n1 + n2;
 
-    while(i <n1 && j<n2) {
-        if(a[i] < b[j]) {
-            if(count == ind1) ind1ele = a[i];
-            if(count == ind2) ind2ele = a[i];
-            count++ ;
-            i++ ;
+        int low = 0;
+        int high = n1;
+
+        int left = (n1 + n2 + 1) / 2;
+        
+        while(low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+
+            if(mid1 < n1) r1 = a[mid1];
+            if(mid2 < n2) r2 = b[mid2];
+
+            if(mid1 - 1 >= 0 ) l1 = a[mid1-1];
+            if(mid2-1 >=0) l2 = b[mid2-1];
+
+            if(l1 <= r2 && l2 <= r1) {
+                if(n%2 == 1) return max(l1, l2);
+                return (  max(l1,l2) + min(r1,r2)) / 2.0; 
+            }
+            else if( l1 > r2) high = mid1-1;
+            else low = mid1+1;
         }
-        else {
-            if(count == ind1) ind1ele = b[j];
-            if(count == ind2) ind2ele = b[j];
-            count++ ;
-            j++ ;
-        }
-    }
-    while(i < n1) {
-        if(count == ind1) ind1ele = a[i];
-        if(count == ind2) ind2ele = a[i];
-        count++ ;
-        i++;
-    }
-    while(j<n2) {
-        if(count == ind1) ind1ele = b[j];
-        if(count == ind2) ind2ele = b[j];
-        count++ ;
-        j++ ;
-    }
-    
-    if(n%2 == 1) {
-        return ind2ele;
-    }
-
-    return ( ind1ele + ind2ele ) / 2.0;
-
-
+        return 0;
     }
 };
