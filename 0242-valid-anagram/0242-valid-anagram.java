455bf5e09dae72f2a9516136e550332a9164bd02
Surya V <mrstudieshelper@gmail.com>
2026-07-09 21:08:02 +0530

    Time: 5 ms (61.61%), Space: 43.8 MB (98.9%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n1 = s.length();
+        int n2 = t.length();
+
+        if(n1 != n2) return false;
+
+        int count[] = new int[26];
+
+        for(int i=0; i<n1 ; i++) {
+            count[s.charAt(i) - 'a']++ ;
+            count[t.charAt(i) - 'a']-- ;
+        }
+
+        for(int value : count) {
+            if(value !=0) return false;
+        }
+        return true;
+    }
+}
