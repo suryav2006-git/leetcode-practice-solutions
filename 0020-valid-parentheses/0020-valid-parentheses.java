8f9a9590b51f4b2b1aa4ce06f8e9127de7023ba5
Surya V <mrstudieshelper@gmail.com>
2026-07-11 11:08:05 -0700

    Time: 141 ms (5.76%), Space: 47.1 MB (5.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
 
+        if(s.length() % 2 == 1) return false;
+
+        if(s.charAt(0) == ')' || s.charAt(0) == '}' || s.charAt(0) == ']') {
+            return false;
+        }
+
         while(true) {
             if( s.contains("()") ) {
                 s = s.replace("()", "");
