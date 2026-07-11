3e866c5e673e73eaffaece51520daf7f78ec70e2
Surya V <mrstudieshelper@gmail.com>
2026-07-11 11:14:18 -0700

    Time: 2 ms (78.51%), Space: 7.6 MB (88.52%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        
+        int first = 0;
+        int second = 1;
+        int current = 0;
+        
+        for (int i = 2; i <= n; i++) {
+            current = second + first;
+            first = second;
+            second = current;
+        }
+        
+        return current;
+    }
+};
