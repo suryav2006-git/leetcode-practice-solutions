f297e130d6b8220b3060f44b9485d108490e8b49
Surya V <mrstudieshelper@gmail.com>
2026-07-11 21:52:39 -0700

    Time: 3 ms (87.01%), Space: 43.2 MB (73.27%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
+
+        int n = s.length();
+
+        if(n == 0)return true;
+
+        if (n % 2 == 1) return false;
+
         Deque<Character> stack = new ArrayDeque<>();
 
         for(char c : s.toCharArray()) {
