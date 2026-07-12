01bafda4d2cef149a2a633a414f74be4179278aa
Surya V <mrstudieshelper@gmail.com>
2026-07-11 21:45:36 -0700

    Time: 3 ms (87.01%), Space: 43.1 MB (85.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
 
-        if(s.length() % 2 == 1) return false;
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                stack.push(')');
+            }
+            else if (c == '[') {
+                stack.push(']');
+            }
+            else if (c == '{') {
+                stack.push('}');
+            }
 
-        if(s.charAt(0) == ')' || s.charAt(0) == '}' || s.charAt(0) == ']') {
-            return false;
-        }
-
-        while(true) {
-            if( s.contains("()") ) {
-                s = s.replace("()", "");
+            else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
             }
-            else if (s.contains("{}")) {
-                s = s.replace("{}","");
-            }
-            else if (s.contains("[]")) {
-                s = s.replace("[]","");
-            }
-            else {
-                return s.isEmpty();
-            }
-        }
-
+        } 
+        return stack.isEmpty();
     }
 }
