40e8b43b1b90692eb3d15eb387d136329cf54037
Surya V <mrstudieshelper@gmail.com>
2026-07-25 00:12:29 -0700

    Time: 0 ms (100%), Space: 13.5 MB (5.41%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -5,18 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        n = len(nums)
         low = 0
-        high = n-1
+        high = len(nums) - 1
 
-        while(low<=high) :
-            mid = (low + high ) // 2
-            if ( nums[mid] == target ) :
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if nums[mid] == target:
                 return mid
-            elif (target > nums[mid]) :
+            elif nums[mid] < target:
                 low = mid + 1
-            else :
+            else:
                 high = mid - 1
+                
         return -1
-        
-        
