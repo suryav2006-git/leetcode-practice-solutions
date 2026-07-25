028e4fe59483305742bfa971c72e8d0cb651389a
Surya V <mrstudieshelper@gmail.com>
2026-07-25 02:05:20 -0700

    Time: 0 ms (100%), Space: 71.8 MB (53.04%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,13 +6,12 @@ public:
         int sum = 0;
         int maxi = INT_MIN;
 
-        for(int i=0; i<n ; i++) {
-            sum += arr[i];
+        for(int val : arr) {
+            sum += val;
+            maxi = max(maxi, sum);
 
-            if(sum > maxi) maxi = sum;
-            if(sum < 0) sum = 0; 
+            if(sum < 0) sum = 0;
         }
-        
 
         return maxi;
         
