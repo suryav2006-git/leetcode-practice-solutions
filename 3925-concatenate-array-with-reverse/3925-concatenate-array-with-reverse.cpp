6332df197964a9520fb0402a98b41c5a3c070604
Surya V <mrstudieshelper@gmail.com>
2026-07-31 08:47:11 +0530

    Time: 2 ms (28.38%), Space: 30.5 MB (55.52%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
@@ -3,13 +3,14 @@ public:
     vector<int> concatWithReverse(vector<int>& arr) {
         int n = arr.size();
 
-        vector<int> ans(2*n) ;
+        vector<int> ans;
 
-        for(int i=0; i<n ; i++) {
-            ans[i] = arr[i];
+        ans.reserve(n * 2);
 
-            ans[i+n] = arr[n-1 -i] ;
-        }
+        ans.assign(arr.begin(), arr.end());
+
+        ans.insert(ans.end() , arr.rbegin(), arr.rend() );
+
         return ans;
     }
 };
