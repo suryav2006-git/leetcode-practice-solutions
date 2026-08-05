58fce1c6aaade5dabfc4eab5939fbe801f33daa2
Surya V <mrstudieshelper@gmail.com>
2026-08-05 18:13:25 +0530

    Time: 1 ms (93.78%), Space: 45.3 MB (88.33%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int once = 0;
+        int twice = 0;
+        for(int num : nums) {
+            if(num <= 9) once += num;
+            else twice += num;
+        }
+        return (once > twice) || (twice > once);
+    }
+}
