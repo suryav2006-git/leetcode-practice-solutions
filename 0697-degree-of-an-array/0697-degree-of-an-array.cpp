fc89350d851882ff5ede667ae4ed85ae386568ce
Surya V <mrstudieshelper@gmail.com>
2026-08-13 19:09:12 +0530

    Time: 7 ms (85.96%), Space: 29.4 MB (72.99%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.cpp
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int minOf(int a, int b) {
-        if(a <= b) return a;
+        if(a<=b) return a;
         return b;
     }
+    
     int findShortestSubArray(vector<int>& nums) {
         unordered_map<int,int> count;
         unordered_map<int,int> firstSeen;
 
-        int maxCount =0; int n = nums.size();
-        int length = 1;
+        int maxCount = 0; int length = 1; int n= nums.size(); 
 
-        for(int i=0; i<n; i++) {
-            if(!firstSeen[nums[i]]) firstSeen[nums[i]] = i+1;
+        for(int i=0; i<n ; i++) {
+            if(!(firstSeen[nums[i]])) firstSeen[nums[i]] = i+1;
 
             count[nums[i]]++ ;
 
             if(count[nums[i]] > maxCount) {
                 maxCount = count[nums[i]];
-                length = (i+1) - firstSeen[nums[i]] + 1;
+                length = (i+1) - firstSeen[nums[i]] +1;
             }
-            else if(count[nums[i]] == maxCount) {
+            else if (count[nums[i]] == maxCount) {
                 length = minOf(length, (i+1) - firstSeen[nums[i]] +1);
             }
         }
