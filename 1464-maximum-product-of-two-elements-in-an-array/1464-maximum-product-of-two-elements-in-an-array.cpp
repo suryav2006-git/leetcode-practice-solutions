b4226e868cad68f05c36137a1294e7c74aede1d9
Surya V <mrstudieshelper@gmail.com>
2026-08-15 08:44:30 -0700

    Time: 0 ms (100%), Space: 13.5 MB (20.08%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -10,7 +10,7 @@ public:
                 max2 = max1;
                 max1 = nums[i];
             }
-            else if(nums[i] > max2) {
+            else if(nums[i] >= max2) {
                 max2 = nums[i];
             }
 
