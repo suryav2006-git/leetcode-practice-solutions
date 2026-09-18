d8f9690f4489c3b0808c927829d9986c3a81d216
Surya V <mrstudieshelper@gmail.com>
2026-09-18 09:19:26 +0530

    Time: 0 ms (100%), Space: 8.5 MB (62.53%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -0,0 +1,21 @@
+bool isPerfectSquare(int num) {
+     if(num < 1) return false;
+
+     long long low = 1;
+     long long high = num;
+
+     while(low <= high) {
+        long long mid = low + (high - low) / 2;
+        long long sqr = mid * mid;
+
+        if(sqr == num) return true;
+        else if(sqr > num) high = mid -1;
+        else low = mid + 1;
+    }
+    return false;
+
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
