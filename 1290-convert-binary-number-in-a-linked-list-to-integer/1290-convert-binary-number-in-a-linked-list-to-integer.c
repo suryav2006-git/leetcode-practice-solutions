cecbf7b6fc45e0e18a0de914ae3fb17ac9198f06
Surya V <mrstudieshelper@gmail.com>
2026-09-21 12:30:37 +0530

    Time: 0 ms (100%), Space: 9 MB (67.7%) - LeetHub

---
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+int getDecimalValue(struct ListNode* head) {
+    int result = 0;
+    struct ListNode *current = head;
+
+    while(current != NULL) {
+        result = (result << 1) | current->val;
+        current = current->next;
+    }
+    return result;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
